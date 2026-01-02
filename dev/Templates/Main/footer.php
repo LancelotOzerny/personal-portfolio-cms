ad940385f621543f460e47f44e2a4412b4f11fd4
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-01-02 19:39:57 +0300

    Вынес header и footer страницы

---
+++ dev/Templates/Main/footer.php
@@ -0,0 +1,21 @@
+<footer class="page__block page__block--spaced theme--dark">
+    <div class="page__container">
+        <div class="main-footer">
+            <div>
+                <div class="section-title__wrapper">
+                    <p class="section-title section-title blue small hashtag">Контакты</p>
+                </div>
+                <div class="networks-line"><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/github.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/gmail.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/telegram.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/vk.svg"></a></div>
+            </div>
+            <div>
+                <div class="section-title__wrapper">
+                    <p class="section-title section-title blue small hashtag">Навигация</p>
+                </div>
+                <div class="main-footer__navigation"><a href="#">Дипломы и сертификаты</a><a href="#">Немного о себе</a><a href="#">Навыки и стек</a><a href="#">Портфолио</a><a href="#">Наверх</a></div>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="assets/scripts/script.js"></script>
+</body>
+</html>
---
+++ dev/Templates/Main/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Портфолио WEB-Разработчика</title>
+    <link rel="stylesheet" href="assets/css/main.css">
+    <link rel="stylesheet" href="assets/css/components.css">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body class="page">
+<header class="page-block">
+    <div class="page__container">
+        <div class="main-navigation">
+            <div class="main-navigation__toggler-wrapper"><button class="main-navigation__toggler">&#9776;</button></div>
+            <div class="navbar"><a class="navbar__link">Обо мне</a><a class="navbar__link">навыки</a><a class="navbar__link">образование</a><a class="navbar__link">портфолио</a></div>
+            <div class="networks-line"><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/github.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/gmail.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/telegram.svg"></a><a class="networks-line__item" href="#"><img class="networks-line__icon" src="assets/icons/vk.svg"></a></div>
+        </div>
+    </div>
+</header>
+<div class="page__block page__block--spaced theme--blue">
+    <div class="page__container">
+        <header class="main-header">
+            <h1 class="main-header__title">Беляков Максим Александрович</h1>
+            <div class="main-header__middle-wrapper">
+                <p class="main-header__jobname">Game Designer • Web Developer • Creative Director</p>
+                <p class="main-header__slogan">"Создаю сайты и виртуальные миры"</p>
+            </div>
+            <div class="main-header__buttons"><a class="btn btn--white btn--circle btn--medium" href="#">Скачать CV</a><a class="btn btn--white btn--circle btn--medium" href="#">Мои проекты</a>
+            </div>
+            <div class="main-header__profile"><img src="assets/images/profile.png"></div>
+        </header>
+    </div>
+</div>
