97fc4a01a481afc0f8de22036b0260145d9f77a9
LancelotOzerny <lancelot.ozernuy@gmail.com>
2026-01-03 03:35:49 +0300

    Поправил контейнер и добавил пару проверок

--- dev/Components/SkillList/Templates/Default/view.php
+++ dev/Components/SkillList/Templates/Default/view.php
@@ -2,6 +2,7 @@
 $right_side = false;
 ?>
 
+<?php if(isset($this->params['areas']) && count($this->params['areas']) > 0): ?>
 <div class="skill-areas-list">
     <?php foreach ($this->params['areas'] as $area): ?>
         <div class="skill-area <?= $right_side ? 'right' : '' ?>">
@@ -10,19 +11,23 @@ $right_side = false;
                 <?= $area['description'] ?>
             </div>
 
+            <?php if(isset($area['skills']) && count($area['skills']) > 0): ?>
             <div class="skill-area__list">
                 <?php foreach ($area['skills'] as $skill): ?>
-                <div class="skill-progress__wrapper">
-                    <div class="skill-progress">
-                        <p class="skill-progress__title"><?= $skill['name'] ?></p>
-                        <div class="skill-progress__line">
-                            <div class="skill-progress__fill"></div>
+                    <div class="skill-progress__wrapper">
+                        <div class="skill-progress">
+                            <p class="skill-progress__title"><?= $skill['name'] ?></p>
+                            <div class="skill-progress__line">
+                                <div class="skill-progress__fill"></div>
+                            </div>
+                            <div class="skill-progress__handler"><?= $skill['progress'] ?>%</div>
                         </div>
-                        <div class="skill-progress__handler"><?= $skill['progress'] ?>%</div>
                     </div>
-                </div>
                 <?php endforeach; ?>
             </div>
+            <?php endif; ?>
         </div>
-    <?php $right_side = !$right_side; ?>
+        <?php $right_side = !$right_side; ?>
     <?php endforeach; ?>
+</div>
+<?php endif; ?>
